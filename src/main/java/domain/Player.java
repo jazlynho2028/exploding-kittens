b62e84d00d9de6610cbcf2cdf5748ec57d794c85
Dong Joo Kwon <dongjoo7@outlook.com>
2026-05-07 20:48:05 -0500

    Created shell of Player.java class file

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Player {
+
+    public Player() {
+
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public void addCardtoHand() {
+
+    }
+
+    public void removeCardFromHand() {
+
+    }
+}
