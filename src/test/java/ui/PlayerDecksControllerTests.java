d3d329fe4605d62fb843a7256548df4416dda181
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2026-05-09 13:17:05 -0500

    Implemented onNameTag_playerStaysTheSame_noChange

--- src/test/java/ui/PlayerDecksControllerTests.java
+++ src/test/java/ui/PlayerDecksControllerTests.java
@@ -8,43 +8,18 @@ public class PlayerDecksControllerTests {
 
 	@Test
 	public void onNameTag_playerStaysTheSame_noChange() {
-	//		Game model = EasyMock.createMock(Game.class);
-	//		AssetProvider assets = EasyMock.createMock(AssetProvider.class);
-	//		EasyMock.expect(model.getCurrentPlayerIndex()).andReturn(0);
-	//
-	//		EasyMock.replay(model);
-	//
-	//		PlayerDeckController controller = new PlayerDeckController(model, assets);
-	//		int playerIndex = 0;
-	//
-	//		controller.onNameTag(playerIndex);
-	//
-	//		EasyMock.verify(model);
-	}
+		Game model = EasyMock.createMock(Game.class);
+		PlayerDeckView view = EasyMock.createMock(PlayerDeckView.class);
+		EasyMock.expect(model.getCurrentPlayerIndex()).andReturn(0);
 
-	@Test
-	public void onNameTag_playerChanges_success() {
-	//		Game model = EasyMock.createMock(Game.class);
-	//		AssetProvider assets = EasyMock.createMock(AssetProvider.class);
-	//
-	//		PlayerDeckController controller = EasyMock.createMockBuilder(
-	//				PlayerDeckController.class
-	//				)
-	//				.withConstructor(model, assets)
-	//				.addMockedMethod("handleChangeCurrentPlayer")
-	//				.createMock();
-	//		int playerIndex = 0;
-	//
-	//		EasyMock.expect(model.getCurrentPlayerIndex()).andReturn(1);
-	//
-	//		controller.handleChangeCurrentPlayer(playerIndex);
-	//		EasyMock.expectLastCall();
-	//
-	//		EasyMock.replay(model, controller);
-	//
-	//		controller.onNameTag(playerIndex);
-	//
-	//		EasyMock.verify(model, controller);
+		EasyMock.replay(model);
+
+		PlayerDeckController controller = new PlayerDeckController(model, view);
+		int playerIndex = 0;
+
+		controller.onNameTag(playerIndex);
+
+		EasyMock.verify(model);
 	}
 
 }
