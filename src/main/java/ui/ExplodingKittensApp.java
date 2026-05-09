497d3182fd603791f20b7197e98508039d4088ac
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2026-05-08 21:22:31 -0500

    Fixed EI_EXPOSE_REP2 in ui.ExplodingKittensApp merged SceneManager roles to main app

--- src/main/java/ui/ExplodingKittensApp.java
+++ src/main/java/ui/ExplodingKittensApp.java
@@ -1,16 +1,31 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ExplodingKittensApp extends Application {
 
+    private AssetManager assets = new AssetManager();
+
     @Override
     public void start(Stage stage) {
-        AssetManager assets = new AssetManager();
+        assets.loadGlobalFiles();
 
-        SceneManager sceneManager = new SceneManager(stage, assets);
-        sceneManager.showStartView();
+        StartController controller = new StartController(assets);
+
+        Scene startScene = controller.getStartScene();
+        setScene(startScene, stage);
+    }
+
+    private void setScene(Scene scene, Stage stage) {
+        scene.getStylesheets().add(assets.getStylesheet());
+
+        stage.setScene(scene);
+        stage.setTitle(UIConstants.TITLE);
+        stage.setResizable(false);
+
+        stage.show();
     }
 
     public static void main(String[] args) {
