288b1ec40eaed5d2e8b8a26017b2bc74112c6a12
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2026-05-13 17:17:02 -0500

    Removed unused import in view

--- src/main/java/ui/PlayerCreateView.java
+++ src/main/java/ui/PlayerCreateView.java
@@ -5,7 +5,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.shape.SVGPath;
