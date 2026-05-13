93345402e2903ca044d435f06375287c264d514d
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2026-05-13 16:41:33 -0500

    Removed unused import in view

--- src/main/java/ui/PlayerCreateView.java
+++ src/main/java/ui/PlayerCreateView.java
@@ -2,7 +2,6 @@ package ui;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
