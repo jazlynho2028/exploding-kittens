253a4e8c12fa7d9052255a1570d52cc329401c04
Dong Joo Kwon <dongjoo7@outlook.com>
2026-05-22 13:33:16 -0500

    getCurrentPlayerIndex_maximumPlayers_returnsZero passes

--- src/test/java/domain/TurnManagerTests.java
+++ src/test/java/domain/TurnManagerTests.java
@@ -53,4 +53,28 @@ public class TurnManagerTests {
 
         EasyMock.verify(mockPlayer1, mockPlayer2, mockPlayer3);
     }
+
+    @Test
+    public void getCurrentPlayerIndex_maximumPlayers_returnsZero() {
+        final int firstPlayerIndex = 0;
+        List<Player> players = new ArrayList<>();
+        Player mockPlayer1 = EasyMock.createMock(Player.class);
+        Player mockPlayer2 = EasyMock.createMock(Player.class);
+        Player mockPlayer3 = EasyMock.createMock(Player.class);
+        Player mockPlayer4 = EasyMock.createMock(Player.class);
+
+        players.add(mockPlayer1);
+        players.add(mockPlayer2);
+        players.add(mockPlayer3);
+        players.add(mockPlayer4);
+
+        EasyMock.replay(mockPlayer1, mockPlayer2, mockPlayer3, mockPlayer4);
+
+        TurnManager turnManager = new TurnManager(players);
+        int actual = turnManager.getCurrentPlayerIndex();
+
+        assertEquals(firstPlayerIndex, actual);
+
+        EasyMock.verify(mockPlayer1, mockPlayer2, mockPlayer3, mockPlayer4);
+    }
 }
