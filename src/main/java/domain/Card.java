8505e67edbe0b81ba439893cea5ad0358b3d5d72
Luke Meyer <lmeyer0215@gmail.com>
2026-05-23 21:32:44 -0500

    Added constructor for id and CardType type, created getter methods for them

--- src/main/java/domain/Card.java
+++ src/main/java/domain/Card.java
@@ -1,6 +1,20 @@
 package domain;
 
 public class Card {
+    private final String id;
+    private final CardType type;
 
+    public Card(String id, CardType type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public CardType getType() {
+        return type;
+    }
 
 }
