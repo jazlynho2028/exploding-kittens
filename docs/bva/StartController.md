b936eb0db46e34fba7fd31d5048d03aa6d97922b
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2026-05-26 20:55:55 -0500

    Removed unused test cases and marked TC1 as complete

--- docs/bva/StartController.md
+++ docs/bva/StartController.md
@@ -1,17 +1,5 @@
 ### Method under test: `buildStartScene()`
-- **TC1: this method is called ** ( :x: )
+- **TC1: this method is called ** ( :white_check_mark: )
   - **Name of the test**: buildStartScene_called_success
   - **State of the system**: N/A
   - **Expected output**: called view.bindEnglishPlayButton with onEnglishPlayButton, called view.bindSpanishPlayButton with onSpanishPlayButton, returns view.createStartScene()
-
-### Method under test: `onEnglishPlayButton()`
-- **TC2: this method is called ** ( :x: )
-    - **Name of the test**: onEnglishPlayButton_buttonPressed_success
-    - **State of the system**: N/A
-    - **Expected output**: called onEnglishPlay.run()
-
-### Method under test: `onSpanishPlayButton()`
-- **TC3: this method is called ** ( :x: )
-    - **Name of the test**: onSpanishPlayButton_buttonPressed_success
-    - **State of the system**: N/A
-    - **Expected output**: called onSpanishPlay.run()
--- src/test/java/ui/StartControllerTests.java
+++ src/test/java/ui/StartControllerTests.java
@@ -35,40 +35,4 @@ public class StartControllerTests {
 		EasyMock.verify(view);
 	}
 
-//	@Test
-//	public void onEnglishPlayButton_buttonPressed_success() {
-//		StartView view = EasyMock.createMock(StartView.class);
-//		Runnable onEnglishPlay = EasyMock.createMock(Runnable.class);
-//
-//		onEnglishPlay.run();
-//		EasyMock.expectLastCall();
-//
-//		StartController controller = new StartController(view);
-//		controller.setOnEnglishPlay(onEnglishPlay);
-//
-//		EasyMock.replay(onEnglishPlay);
-//
-//		controller.onEnglishPlayButton();
-//
-//		EasyMock.verify(onEnglishPlay);
-//	}
-//
-//	@Test
-//	public void onSpanishPlayButton_buttonPressed_success() {
-//		StartView view = EasyMock.createMock(StartView.class);
-//		Runnable onSpanishPlay = EasyMock.createMock(Runnable.class);
-//
-//		onSpanishPlay.run();
-//		EasyMock.expectLastCall();
-//
-//		StartController controller = new StartController(view);
-//		controller.setOnSpanishPlay(onSpanishPlay);
-//
-//		EasyMock.replay(onSpanishPlay);
-//
-//		controller.onSpanishPlayButton();
-//
-//		EasyMock.verify(onSpanishPlay);
-//	}
-
 }
