12d767c91c5f16ad2c530c26b097f4e1b904536d
Dong Joo Kwon <dongjoo7@outlook.com>
2026-05-29 13:31:49 -0500

    Parametrized getCanDraw_called_returnFalse test

--- src/test/java/domain/GameTests.java
+++ src/test/java/domain/GameTests.java
@@ -502,10 +502,11 @@ public class GameTests {
 				);
 	}
 
-	@Test
-	public void getCanDraw_called_returnFalse() {
-		final int DRAW_COUNT = 0;
-
+	@ParameterizedTest
+	@CsvSource({
+			"false, 0"
+	})
+	public void getCanDraw_called_returnFalse(boolean isGameOngoing, int drawCount) {
 		Player player1 = EasyMock.createNiceMock(Player.class);
 		Player player2 = EasyMock.createNiceMock(Player.class);
 		List<Player> players = List.of(player1, player2);
@@ -522,8 +523,8 @@ public class GameTests {
 				.addMockedMethod("getDrawCount")
 				.createMock();
 
-		EasyMock.expect(game.getIsGameOngoing()).andReturn(false);
-		EasyMock.expect(game.getDrawCount()).andReturn(DRAW_COUNT).anyTimes();
+		EasyMock.expect(game.getIsGameOngoing()).andReturn(isGameOngoing);
+		EasyMock.expect(game.getDrawCount()).andReturn(drawCount).anyTimes();
 
 		EasyMock.replay(game);
 
