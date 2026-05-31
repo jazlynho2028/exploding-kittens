8aa199e681c183429ea9bf0d62cfef82c7a3932b
Ejean Kuo <ejeankuo2027@u.northwestern.edu>
2026-05-31 14:45:04 -0500

    fix broken calls to deck class

--- src/main/java/ui/ExplodingKittensApp.java
+++ src/main/java/ui/ExplodingKittensApp.java
@@ -71,7 +71,8 @@ public class ExplodingKittensApp extends Application {
         List<Player> players = createPlayers(createController.getConfirmedNames());
         DeckBuilder builder = new DeckBuilder();
         Deck drawPile = builder.initializeDeck(players.size());
-        Deck discardPile = new Deck(new ArrayDeque<>());
+        Random random = new Random();
+        Deck discardPile = new Deck(new ArrayDeque<>(), random);
         TurnManager turnManager = new TurnManager(players);
 
         Game model = new Game(players, drawPile, discardPile, turnManager);
