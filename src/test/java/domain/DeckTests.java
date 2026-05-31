0c9fd0f645a5f44470ce52b36c845aca427fb3b7
Irene Ha <89060602+Sunw00ha@users.noreply.github.com>
2026-05-30 19:06:34 -0500

    Refactor Deck shuffle tests with new runShuffleTest helper

--- src/test/java/domain/DeckTests.java
+++ src/test/java/domain/DeckTests.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import java.util.stream.Stream;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 import java.util.Random;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,11 +29,7 @@ public class DeckTests {
         Random mockRandom = EasyMock.createMock(Random.class);
         EasyMock.replay(mockRandom);
 
-        Deck deck = new Deck(cards, mockRandom);
-
-        deck.shuffle();
-
-        assertEquals(0, deck.size());
+        runShuffleTest(cards, mockRandom, List.of());
 
         EasyMock.verify(mockRandom);
     }
@@ -46,12 +43,7 @@ public class DeckTests {
         Deque<Card> cards = new ArrayDeque<>();
         cards.addLast(card1);
 
-        Deck deck = new Deck(cards, mockRandom);
-
-        deck.shuffle();
-
-        assertEquals(1, deck.size());
-        assertSame(card1, deck.peekTop());
+        runShuffleTest(cards, mockRandom, List.of(card1));
 
         EasyMock.verify(card1, mockRandom);
     }
@@ -70,12 +62,7 @@ public class DeckTests {
         cards.addLast(card2);
         cards.addLast(card1);
 
-        Deck deck = new Deck(cards, mockRandom);
-
-        deck.shuffle();
-
-        assertEquals(2, deck.size());
-        assertSame(card1, deck.peekTop());
+        runShuffleTest(cards, mockRandom, List.of(card1, card2));
 
         EasyMock.verify(card1, card2, mockRandom);
     }
@@ -97,17 +84,30 @@ public class DeckTests {
         cards.addLast(card1CopyTwo);
         cards.addLast(card2);
 
-        Deck deck = new Deck(cards, mockRandom);
+        runShuffleTest(
+                cards,
+                mockRandom,
+                List.of(card1CopyTwo, card2, card1CopyOne));
+
+        EasyMock.verify(card1CopyOne, card1CopyTwo, card2, mockRandom);
+    }
+
+    private void runShuffleTest(
+            Deque<Card> originalCards,
+            Random mockRandom,
+            List<Card> expectedCards) {
+        Deck deck = new Deck(originalCards, mockRandom);
 
         deck.shuffle();
 
-        assertEquals(THREE_CARDS, deck.size());
-        assertSame(card1CopyTwo, deck.removeTop());
-        assertSame(card2, deck.removeTop());
-        assertSame(card1CopyOne, deck.removeTop());
-        assertEquals(0, deck.size());
+        assertEquals(expectedCards.size(), deck.size());
 
-        EasyMock.verify(card1CopyOne, card1CopyTwo, card2, mockRandom);
+        List<Card> actualCards = new ArrayList<>();
+        while (deck.size() > 0) {
+            actualCards.add(deck.removeTop());
+        }
+
+        assertEquals(expectedCards, actualCards);
     }
 
     @Test
